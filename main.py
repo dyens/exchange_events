5950119c16fd4bd156fe990a08af8f1701795c3d
dyens <dyens@mail.ru>
2018-10-14 19:59:29 +0000

    add get events and print events functions

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from exchangelib import DELEGATE, Account, Credentials, Configuration
-from exchangelib import EWSTimeZone
-from exchangelib import EWSDateTime
-from exchangelib import IMPERSONATION
-
-from exchangelib.errors import ErrorMailboxMoveInProgress
-from exchangelib.errors import ErrorMailboxStoreUnavailable
-
 import argparse
+import os
 import re
+
 from dateutil.parser import parse
+
+from exchangelib import (DELEGATE, IMPERSONATION, Account, Configuration,
+                         Credentials, EWSDateTime, EWSTimeZone)
+from exchangelib.errors import (ErrorMailboxMoveInProgress,
+                                ErrorMailboxStoreUnavailable)
 
 ereg = r'[^@]+@[^@]+\.[^@]+'
 
@@ -32,6 +31,52 @@ parser.add_argument('start', help='start events', type=datetime_type)
 parser.add_argument('end', help='end events', type=datetime_type)
 parser.add_argument('emails', nargs='+', type=email_type)
 
+
+def get_events(emails, start, end):
+    credentials = Credentials(
+        username=os.environ.get('EXCHANGE_USER'),
+        password=os.environ.get('EXCHANGE_PASSWORD'))
+    config = Configuration(server='ews.zoloto585.ru', credentials=credentials)
+
+    tz = EWSTimeZone.timezone('Europe/Moscow')
+    events = {}
+    for email in emails:
+        account = Account(
+            primary_smtp_address=email,
+            autodiscover=False,
+            config=config,
+            access_type=IMPERSONATION)
+        start = EWSDateTime(start.year, start.month, start.day, start.hour,
+                            start.minute, start.second)
+        end = EWSDateTime(end.year, end.month, end.day, end.hour, end.minute,
+                          end.second)
+        evs = account.calendar.view(
+            start=tz.localize(start), end=tz.localize(end))
+        events[email] = evs
+    return events
+
+
+def print_event(event):
+    attrs = [
+        'is_cancelled', 'start', 'end', 'uid', 'subject', 'location',
+        'organizer'
+    ]
+
+    for attr in attrs:
+        print(attr, ': ', getattr(event, attr))
+        print('------------------------------')
+    attendees = [
+        attendee.mailbox.email_address for attendee in event.required_attendees
+    ]
+    if attendees:
+        print('ATTENDEES:')
+        print(attendees)
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    print(args)
+    start=args.start
+    end=args.end
+    emails = args.emails
+    events = get_events(emails, start, end)
+    print_event(events)
